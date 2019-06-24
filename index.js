2cdf1d55a647cd9733d4dc44c0e32875fda73926
Miguel Mota <hello@miguelmota.com>
2019-06-23 18:15:59 -0700

    lint fixes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function toChecksumAddress (address, chainId = null) {
     return ''
   }
 
-  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) { throw new Error(`Given address "${address}" is not a valid Ethereum address.`) }
+  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+    throw new Error(`Given address "${address}" is not a valid Ethereum address.`)
+  }
 
   const stripAddress = stripHexPrefix(address).toLowerCase()
   const prefix = chainId != null ? chainId.toString() + '0x' : ''
@@ -14,7 +16,9 @@ function toChecksumAddress (address, chainId = null) {
     .replace(/^0x/i, '')
   let checksumAddress = '0x'
 
-  for (let i = 0; i < stripAddress.length; i++) { checksumAddress += parseInt(keccakHash[i], 16) >= 8 ? stripAddress[i].toUpperCase() : stripAddress[i] }
+  for (let i = 0; i < stripAddress.length; i++) {
+    checksumAddress += parseInt(keccakHash[i], 16) >= 8 ? stripAddress[i].toUpperCase() : stripAddress[i]
+  }
 
   return checksumAddress
 }
@@ -33,11 +37,11 @@ function checkAddressChecksum (address, chainId = null) {
     }
   }
   return true
-};
+}
 
-function stripHexPrefix (string) {
-  return string.slice(0, 2) === '0x' ? string.slice(2) : string
-};
+function stripHexPrefix (value) {
+  return value.slice(0, 2) === '0x' ? value.slice(2) : value
+}
 
 module.exports = {
   toChecksumAddress,
